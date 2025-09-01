3ad2e604c693b14b920d8a1a52d8c1b5b044c104
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-01 09:45:07 +0900

    default props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import MyComponent from './components/MyComponent';
 const App = () => {
   return (
     <>
-      
+      <MyComponent name="ReactJS" />
+      <MyComponent />
+
+
     </>
   );
 };
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 class MyComponent extends Component {
     render() {
+        //destructuring assigment
+        const { name } = this.props;
         return (
             <div>
-                
+                <h2>클래스 타입 컴포넌트</h2>
+                <h3>안녕하세요! <b>{name}</b></h3>
             </div>
         );
     }
 }
-
+MyComponent.defaultProps = {
+    name: '리액트JS'
+};
 export default MyComponent;
