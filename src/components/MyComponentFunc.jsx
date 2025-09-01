004cabe67c8ec87b48d62e85c149f15803044315
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-01 13:07:38 +0900

    MyCompFunc handleChange

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const MyComponentFunc = ({ name, children }) => {
     //useState() 함수사용
@@ -7,14 +7,14 @@ const MyComponentFunc = ({ name, children }) => {
         message: '',
         username: '',
     });
-const { message, username} = inputs;
+    const { message, username } = inputs;
 
-//event handlers
-const handleChange = (e) => {
-    setInputs({
-        [e.target.name]: e.target.value
-    });
-}
+    //event handlers
+    const handleChange = (e) => {
+        setInputs({
+            [e.target.name]: e.target.value
+        });
+    }
     return (
         <div>
             <h2>함수형 컴포넌트</h2>
@@ -23,6 +23,12 @@ const handleChange = (e) => {
             <p>상태변수 value = {value}</p>
             <button onClick={() => (setValue(value + 1))}>증가</button>
             <button onClick={() => (setValue(value - 1))}>감소</button>
+            <br />
+            <p>상태변수 message = {message}</p>
+            <input name="message" value={message} onChange={handleChange} />
+            <p>상태변수 username = {username}</p>
+            <input name="username" value={username} onChange={handleChange} />
+
         </div>
     );
 };
